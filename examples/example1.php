c8399bf22cd54b2c612d5e168792f1cf014a9e71
Mohamed Elbahja <melbahja@users.noreply.github.com>
2016-06-21 13:46:58 +0000

    Create example1.php

---
+++ examples/example1.php
@@ -0,0 +1,30 @@
+<?php
+
+require('image_sizer.php');
+
+$img = new image_sizer();
+
+
+/**
+ * setImage()
+ * url : http://localhost/example1.php?img=https://upload.wikimedia.org/wikipedia/commons/c/c1/PHP_Logo.png
+ * 
+ */
+$img->setImage($_GET['img']);
+
+
+/**
+ * setSize
+ * New image size
+ * $img->setSize(Width, Height)
+ */
+$img->setSize(100, 60);
+
+/**
+ * show image
+ * 
+ * $img->show(type, quality);
+ * type : png, jpeg, jpg, gif
+ * quality : image show quality 100 = 100%
+ */
+$img->show('jpeg', 10);
