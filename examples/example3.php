6f692bc2c7253b4608eb0628cee71ea31b88872c
Mohamed Elbahja <melbahja@users.noreply.github.com>
2016-06-21 13:52:21 +0000

    Create example3.php

---
+++ examples/example3.php
@@ -0,0 +1,36 @@
+<?php
+
+
+require('image_sizer.php');
+
+
+$img = new image_sizer();
+
+
+/**
+ * setImage(path)
+ * path : path/to/image.png
+ * 
+ */
+$img->setImage('php.png');
+
+
+/**
+ * setSize
+ * New image size
+ * $img->setSize(Width, Height)
+ */
+$img->setSize(100, 60);
+
+
+
+/**
+ * saveTo
+ * save image
+ * $img->saveTo(full path and name, quality); 
+ */
+$img->saveTo('new_image.png', 100); // save image png type
+
+//$img->saveTo('new_image.jpg', 100); // save image jpg type
+//$img->saveTo('new_image.jpeg', 100); // save image jpg type
+//$img->saveTo('new_image.gif'); // save image gif type
